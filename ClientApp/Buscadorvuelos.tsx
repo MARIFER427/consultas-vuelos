a3a156cfe9e13f10853602a32bc6321747a234b5
MARIFER427 <mariafernanda.demezabermudez.gen23@cbtis105.edu.mx>
2025-05-14 10:05:57 -0600

    min. 43:18

--- ClientApp/Buscadorvuelos.tsx
+++ ClientApp/Buscadorvuelos.tsx
@@ -1,4 +1,79 @@
+import {useEffect, useState} from "react";
+import ListadoVuelos from "./ListadoVuelos";
+
+type Ciudad = {
+  Nombre: string
+}
+type EstatusVuelo ={
+  Nombre: string
+}
+
+type Vuelo = {
+  PaisOrigen: string,
+  CiudadOrigen: string,
+  AeropuertoOrigen: string,
+  PaisDestino: string,
+  CiudadDestino:string,
+  AeropuertoDestino: string,
+  PasajerosActuales: number,
+  NombrePiloto: string,
+  FechaHoraSalida: string,
+  FechaHoraLllegadaAproximada: String
+}
+
 const BuscadorVuelos = () => {
+  const [ciudadesOrigen, setCiudadesOrigen] = useState<Ciudad[]>([]);
+  const [ciudadesDestino,setCiudadesDestino]= useState<Ciudad[]>([]);
+  const [ListaEstatus, setListaEstatus]= useState<EstatusVuelo[]>([]);
+
+  const listarCiudadesOrigen = async () => {
+    const response = await fetch("http://localhost:5000/api/vuelos/ciudades-origen");
+    if(response.ok) {
+      const arr = await response.json();
+      let ciudades : Array<Ciudad> = [];
+
+      arr.map((x: string) => ciudades.push({Nombre: x}));
+
+     setCiudadesOrigen(ciudades);
+    }
+  }
+  const listarCiudadesDestino= async () => {
+    const response = await fetch("http://localhost:5000/api/vuelos/ciudad-destino");
+    if(response.ok) {
+      const arr = await response.json();
+      let ciudades : Array<Ciudad> = [];
+
+      arr.map((x: string) => ciudades.push({Nombre: x}));
+
+     setCiudadesDestino(ciudades);
+    }
+  }
+  const listarEstatus= async () => {
+    const response = await fetch("http://localhost:5000/api/vuelos/estatus");
+    if(response.ok) {
+      const arr = await response.json();
+      let estatus : Array<EstatusVuelo> = [];
+
+      arr.map((x: string) => estatus.push({Nombre: x}));
+
+     setListaEstatus(estatus);
+    }
+  }
+   const listarVuelos= async () => {
+    const response = await fetch("http://localhost:5000/api/vuelos/listar-vuelos");
+    if(response.ok) {
+      const arr = await response.json();
+      console.log(arr);
+    }
+  }
+  useEffect(()=> {
+    listarCiudadesOrigen();
+    listarCiudadesDestino();
+    listarEstatus();
+  }, []);
+
+
+
   return (
     <> 
     <div className="h1">Buscador de Vuelos</div>
@@ -21,12 +96,48 @@ const BuscadorVuelos = () => {
           <div className="col-sm-4">
             <div className="mb-3">
                <label>Origen</label>
-               <select className="form-control"></div></select>
+               <select className="form-control">
+                <option value="">(Todos)</option>
+                {
+                   ciudadesOrigen.map(x => <option key={x.Nombre} value={x.Nombre}>{x.Nombre}</option>)
+                }
+               </select>
             </div>
+          </div>
+            <div className="col-sm-4">
+            <div className="mb-3">
+               <label>Destino</label>
+               <select className="form-control">
+                 <option value="">(Todos)</option>
+                {
+                   ciudadesDestino.map(x => <option key={x.Nombre} value={x.Nombre}>{x.Nombre}</option>)
+                }
+               </select>
+            </div>
+          </div>
+           <div className="col-sm-4">
+            <div className="mb-3">
+               <label>Estatus</label>
+               <select className="form-control">
+                                <option value="">(Todos)</option>
+                {
+                   ListaEstatus.map(x => <option key={x.Nombre} value={x.Nombre}>{x.Nombre}</option>)
+                }
+               </select>
+            </div>
+          </div>
+          <div className="col-12 d-flex justify-content-end">
+            <button className="btn btn-primary" onClick={()=> {listarVuelos();}}>Buscar</button>
           </div>
         </div>
       </div>
+   </div>
+   <div className="card-mt-4">
+    <div className="card header">Vuelos Encontrados</div>
+    <div className="card-body">
+      <ListadoVuelos/>
     </div>
+   </div>
     </>
    )
 }
